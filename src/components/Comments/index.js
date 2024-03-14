50684c24d5446505e0c3a1db6cc30e032309f609
TanmayGhosh347 <tanmay2690Qgmail.com>
2024-03-14 06:11:53 +0000

    first commit

---
+++ src/components/Comments/index.js
@@ -0,0 +1,64 @@
+import {useState} from 'react'
+import {v4 as uuidv4} from 'uuid'
+import CommentItem from '../CommentItem'
+import {
+  CommentsContainer,
+  CommentsTitle,
+  Form,
+  NameInput,
+  CommentTextInput,
+  CommentButton,
+  CommentsList,
+} from './styledComponents'
+
+const Comments = () => {
+  const [name, setName] = useState('')
+  const [commentText, setCommentText] = useState('')
+
+  const [commentsList, setCommentsList] = useState([])
+
+  const onChangeNameInput = event => {
+    setName(event.target.value)
+  }
+  const onChangeCommentText = event => {
+    setCommentText(event.target.value)
+  }
+
+  const onAddComment = event => {
+    event.preventDefault()
+    const newComment = {
+      id: uuidv4(),
+      name,
+      commentText,
+    }
+    setName('')
+    setCommentText('')
+    setCommentsList(prevCommentList => [...prevCommentList, newComment])
+  }
+  return (
+    <CommentsContainer>
+      <CommentsTitle>Comments</CommentsTitle>
+      <Form onSubmit={onAddComment}>
+        <NameInput
+          type="text"
+          placeholder="Your name"
+          value={name}
+          onChange={onChangeNameInput}
+        />
+        <CommentTextInput
+          placeholder="Your comment"
+          rows="6"
+          value={commentText}
+          onChange={onChangeCommentText}
+        />
+        <CommentButton type="submit">Comment</CommentButton>
+      </Form>
+      <CommentsList>
+        {commentsList.map(eachComment => (
+          <CommentItem commentDetails={eachComment} key={eachComment.id} />
+        ))}
+      </CommentsList>
+    </CommentsContainer>
+  )
+}
+export default Comments
